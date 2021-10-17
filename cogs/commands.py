08a6be0dd90f077bac9da76e2c63afdc7e9e8e53
Anton <kontakt@antonstech.de>
2021-10-17 12:02:06 +0200

    Added an Invite Feature

--- cogs/commands.py
+++ cogs/commands.py
@@ -1,10 +1,11 @@
+import discord
 from discord import Embed
 from discord.ext.commands import Bot, Cog
 from discord_slash import cog_ext, SlashContext
 from utils import utils
 
 
-class Slash(Cog):
+class Commands(Cog):
     def __init__(self, bot: Bot):
         self.bot = bot
         utils.LOGGER.debug(f"Successfully loaded cog {self.__class__.__name__}")
@@ -14,6 +15,14 @@ class Slash(Cog):
         embed = Embed(title="Embed Test")
         await ctx.send(embed=embed)
 
+    @cog_ext.cog_slash(name="invite", description="Lets you invite the Bot")
+    async def _bot_invite(self, ctx: SlashContext):
+        embed = discord.Embed()
+        embed.set_author(name="Press the Link to invite the bot",
+                         url=discord.utils.oauth_url(self.bot.user.id, permissions=discord.Permissions(8),
+                                                     guild=ctx.guild, scopes=["bot", "applications.commands"]))
+        await ctx.send(embed=embed)
+
 
 def setup(bot: Bot):
-    bot.add_cog(Slash(bot))
+    bot.add_cog(Commands(bot))
