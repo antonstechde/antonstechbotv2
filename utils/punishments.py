d64f9559985babc47837c4dbd8de0892f701ac19
Ed_Vraz <edvraz12@gmail.com>
2021-11-09 10:11:15 +0100

    Mute buttons
    - timeout added
    - file reformat
    - changed button labels from int to str

--- utils/punishments.py
+++ utils/punishments.py
@@ -2,5 +2,18 @@ import discord
 
 # handle ban/kick/warn/mute commands
 
+
 async def mute(ctx, user, length: int, mhd: str):
+    ...
+
+
+async def warn(ctx, user, reason):
+    ...
+
+
+async def kick(ctx, user, reason):
+    ...
+
+
+async def ban(ctx, user, reason):
     ...
